e45d02e2059de6176534253bbb0db37b90b99a74
Andrew Macmillan <andrew.macmillan@ymail.com>
2022-05-02 16:42:34 +0100

    complete basic test suite

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,13 +1,15 @@
 import argparse
 import os
+import socket
 import unittest
 
 from lobbit.client import (
     check_parse_errors,
     get_parser,
+    LobbitClient,
     valid_ip,
-    valid_port,
-    valid_path
+    valid_path,
+    valid_port
 )
 
 
@@ -23,6 +25,15 @@ class TestClient(unittest.TestCase):
         self.parser = get_parser()
         self.good_path = f"{os.path.abspath(os.path.dirname(__file__))}/blank"
         self.bad_path = f"{os.path.abspath(os.path.dirname(__file__))}/tank"
+        self.lc = LobbitClient("172.16.0.10", 1234, [self.good_path])
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def tearDown(self) -> None:
+        """
+        Cleans up after testing
+        """
+        self.lc.sock.close()
+        self.sock.close()
 
     def test_valid_ip_returns_ip(self) -> None:
         """
@@ -109,3 +120,33 @@ class TestClient(unittest.TestCase):
             args = self.parser.parse_args(["-i", "192.168.0.10", "-p", "1234", "-f", self.bad_path])
             check_parse_errors(self.parser, args)
             self.assertEqual(se.exception.code, 2)
+
+    def test_check_parse_errors_catch_empty_files_list(self) -> None:
+        """
+        Tests that the check_parse_errors function raises an error if
+        the user doesn't pass in any file names to send
+        """
+        with self.assertRaises(SystemExit) as se:
+            args = self.parser.parse_args(["-i", "192.168.0.10", "-p", "1234"])
+            check_parse_errors(self.parser, args)
+            self.assertEqual(se.exception.code, 2)
+
+    def test_LobbitSocket_initialises_correctly(self) -> None:
+        """
+        Tests that a newly created instance of LobbitSocket has the correct
+        attributes
+        """
+        self.assertEqual(self.lc.ip, "172.16.0.10")
+        self.assertEqual(self.lc.port, 1234)
+        self.assertEqual(self.lc.files, [self.good_path])
+        self.assertEqual(type(self.lc.sock), type(self.sock))
+        self.assertEqual(self.lc.delimiter, ":")
+        self.assertEqual(self.lc.buffer_size, 4096)
+
+    def test_get_file_sizes_returns_dictionary_with_correct_sizes(self) -> None:
+        """
+        Tests that filenames and sizes are sent back in the correct format
+        """
+        file_sizes = self.lc.get_file_sizes()
+        expected = {"/storage/data/My_Work/Programming/Python/Python Projects/cmd_tools/lobbit/tests/blank": 0}
+        self.assertEqual(file_sizes, expected)
