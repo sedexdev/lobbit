3d5402ed2a914506a776b23a67d525408f10ec34
Andrew Macmillan <=>
2022-08-17 10:04:41 +0100

    implement LobbitHash and test class

--- app/lobbit_util/crypto.py
+++ app/lobbit_util/crypto.py
@@ -1,5 +1,7 @@
 import hashlib
-import os
+import string
+
+from typing import Tuple
 
 
 class LobbitHash:
@@ -7,29 +9,66 @@ class LobbitHash:
     Class that exposes a few basic hash related methods
     """
 
-    def create_hash(self, value: bytes) -> str:
+    def __init__(self, value: bytes, salt: bytes):
+        """
+        Constructor for the LobbitHash class
+
+        Args:
+            value (bytes) : the users password
+            salt (bytes)  : salt to add to the hash
+        """
+        self.value = value
+        self.salt = salt
+
+    def create_hash(self) -> Tuple:
         """
         Takes in a byte string and generates a 256bit hash digest
         using the SHA-256 hash function. The salt is pulled from
         the database and used to compare the test_data in <value> to
         the value stored in the database
 
-        Args:
-            value (bytes) : byte string to be hashed
         Returns:
             str : the hash digest of the value
         """
-        pass
+        verified = self.verify_pw()
+        if not verified:
+            return "Password complexity requirements not met. See help for more details",
+        return self.salt, hashlib.sha3_256(self.value + self.salt).hexdigest()
 
-    def verify_hash(self, x: str, y: str) -> bool:
+    def verify_pw(self) -> bool:
         """
+        Verifies that the password matches complexity requirements.
+        The password must:
+            - be at least 12 characters long
+            - contain a lowercase and uppercase letter
+            - contain a numerical character
+            - contain at least 3 symbols
+        """
+        if len(self.value) < 12:
+            return False
+        if not any(i for i in self.value.decode() if i.islower()):
+            return False
+        if not any(i for i in self.value.decode() if i.isupper()):
+            return False
+        if not any(i for i in self.value.decode() if i.isdigit()):
+            return False
+        if len([i for i in string.punctuation if i in self.value.decode()]) < 3:
+            return False
+        return True
+
+    def verify_hash(self, users_hash: str) -> bool:
+        """
+        Compares the salted hash value of the password provided by the
+        user to the one stored against the user in the database
+
         Args:
-            x (str) : the hash of the value sent by the user
-            y (str) : the stored hash value to compare x against
+            users_hash (str) : existing hash of users salted password
+
         Returns:
-            bool : boolean verifying whether the 2 values x and y match
+            bool : boolean verifying whether the passwords match
         """
-        pass
+        pw_hash = hashlib.sha3_256(self.value + self.salt).hexdigest()
+        return pw_hash == users_hash
 
 
 class LobbitEncrypt:
